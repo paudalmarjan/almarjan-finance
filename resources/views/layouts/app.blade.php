55ca1c2c7c0f5e3d2e59c78a95f47de92866ee9a
wafiqj <jaisyurrahmanwafiq@gmail.com>
2026-06-18 06:39:03 +0700

    update favicon and title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'PAUD Al Marjan') }}</title>
+    <title>@hasSection('title') @yield('title') | @endif{{ config('app.name', 'PAUD Al Marjan') }}</title>
+
+    <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
 
     <!-- Scripts and Styles -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
